795564c28e6d3eb8dc87980bd695d2573d18b859
liuwanjie <530680853@qq.com>
2016-02-18 13:02:29 +0800

    项目初始化

--- src/main/java/com/beacon/wechat/Application.java
+++ src/main/java/com/beacon/wechat/Application.java
@@ -7,11 +7,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
+@ImportResource("classpath:config/spring.xml")
 public class Application extends SpringBootServletInitializer{
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
