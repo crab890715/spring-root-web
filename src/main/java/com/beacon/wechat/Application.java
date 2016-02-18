91289db048d00bc884846251428a7935282c0da8
liuwanjie <530680853@qq.com>
2016-02-18 12:44:35 +0800

    项目初始化

--- src/main/java/com/beacon/wechat/Application.java
+++ src/main/java/com/beacon/wechat/Application.java
@@ -2,14 +2,23 @@ package com.beacon.wechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer{
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
 }
--- src/main/java/com/beacon/wechat/controller/DemoController.java
+++ src/main/java/com/beacon/wechat/controller/DemoController.java
@@ -1,12 +1,14 @@
 package com.beacon.wechat.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController 
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller 
 @RequestMapping("/demo")
+//@Controller
 public class DemoController {
-	@RequestMapping("/demo") 
+	@RequestMapping(value="/demo",method=RequestMethod.GET) 
 	public String demo() { 
-		return "我是测试";
+		return "index";
 	}
 }
